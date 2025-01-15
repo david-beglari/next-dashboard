93d0d2352eb447b7ca2e87496a1ecd963e405e18
David <davit.khachatryan@dataart.com>
2025-01-15 17:58:36 +0400

    Created dashboard functionality

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  async rewrites() {
+    return [
+      { source: "/dashboard", destination: "/ui/dashboard" },
+      { source: "/dashboard/invoices", destination: "/ui/invoices" },
+      { source: "/dashboard/customers", destination: "/ui/customers" },
+    ];
+  },
 };
 
 export default nextConfig;
